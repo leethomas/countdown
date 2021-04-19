4d1f562d5cf9b39a2eb01d964d6076d9864750ef
Lee <leet944@gmail.com>
2021-04-19 13:16:20 -0700

    fix config path resolution

--- main.rs
+++ main.rs
@@ -1,5 +1,6 @@
 use std::convert::TryFrom;
 use std::time::{Duration, SystemTime, UNIX_EPOCH};
+use std::path::PathBuf;
 
 const SECONDS_IN_DAY: u64 = 86400;
 const CONFIG_FILENAME: &str = ".countdown.yml";
@@ -40,21 +41,16 @@ fn main() {
   let result: Result<Vec<(i32, String)>, String> =
     dirs::home_dir()
       .ok_or_else(|| "Failed to find home directory.".to_string())
-      .map(|home| format!("{:?}/{:?}", home.to_str(), CONFIG_FILENAME))
-      .and_then(|config_file: String| {
-        println!("config file: {:?}", config_file);
-        confy::load_path(config_file.as_str())
-          .map_err(|_| "Couldn't load config.".to_string())})
-      .map(|config: CountdownConfig| {
-        println!("config: {:?}", config);
-        config.events
+      .map(|home| [home, CONFIG_FILENAME.into()].iter().collect::<PathBuf>())
+      .and_then(|config_file| confy::load_path(config_file)
+        .map_err(|e| format!("Couldn't load config: {:?}", e).to_string()))
+      .map(|config: CountdownConfig| config.events
         .iter()
         .map(|ev| match ev.days_left(now) {
-          Ok(days) => (i32::from(days), format!("{:?} days until {:?}", days, ev.name)),
+          Ok(days) => (i32::from(days), format!("{} days until {}", days, ev.name)),
           Err(e) => (-1, (format!("countdown: {:?}", e))),
-        }).collect()});
+        }).collect());
 
-  println!("result: {:?}", result);
   match result  {
       Ok(all_events) => {
         let (mut valid, invalid): (Vec<(i32, String)>, Vec<(i32, String)>) =
@@ -63,7 +59,7 @@ fn main() {
         invalid.iter().for_each(|(_, msg)| eprintln!("{:?}", msg));
         valid
           .sort_by(|(a_days, _), (b_days, _)| a_days.cmp(b_days));
-        valid.iter().for_each(|(_, msg)| println!("{:?}", msg))
+        valid.iter().for_each(|(_, msg)| println!("{}", msg))
       },
       Err(e) => eprintln!("{:?}", e),
     }
